84e5dcf456207440c57a9592788daec916d0034d
Romain Goyet <romain.goyet@likid.org>
2010-07-14 22:35:43 +0200

    Auto-combining NameCleaner !

--- NameCleaner.rb
+++ NameCleaner.rb
@@ -3,16 +3,80 @@ require 'rubygems'
 require 'nokogiri'
 require 'Text'
 
-algorithms = {}
+$building_blocks = {}
 
-algorithms['Naive'] = Proc.new do |fileName|
-  fileName
+$building_blocks['NoExt'] = Proc.new do |fileName|
+  (/^(.*)\.([a-z]){2,4}$/i.match(fileName)||['', fileName])[1]
 end
 
-algorithms['RemoveExtension'] = Proc.new do |fileName|
-  fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
-  fileNameWithoutExtension
+$building_blocks['NoExt2'] = Proc.new do |fileName|
+  (/^(.*)\.([a-z]){2,4}$/i.match(fileName)||['', fileName])[1]
 end
+
+$building_blocks['NoExt3'] = Proc.new do |fileName|
+  (/^(.*)\.([a-z]){2,4}$/i.match(fileName)||['', fileName])[1]
+end
+
+
+
+$algorithms = []
+
+def combine_building_blocks(available_block_names, current_block_stack)
+  $algorithms << current_block_stack
+  available_block_names.each do |block|
+    combine_building_blocks(available_block_names.reject{|block_name| block_name == block}, current_block_stack + [block])
+  end
+end
+
+combine_building_blocks($building_blocks.keys, [])
+
+# algorithms['Naive'] = Proc.new do |fileName|
+#   fileName
+# end
+# 
+# algorithms['RemoveExtension'] = Proc.new do |fileName|
+#   fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
+#   fileNameWithoutExtension
+# end
+# 
+# 
+# algorithms['RemoveExtensionAndRenameDots'] = Proc.new do |fileName|
+#   fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
+#   dots_removed = fileNameWithoutExtension.gsub('.', ' ')
+#   everything_before_bad_keyword = (/^(.*)(FRENCH|DVDRIP|XVID|DIVX)(.*)$/i.match(dots_removed) || ['', dots_removed])[1]
+#   everything_before_bad_keyword
+# end
+# 
+# algorithms['R3'] = Proc.new do |fileName|
+#   fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
+#   dots_removed = fileNameWithoutExtension.gsub('.', ' ')
+#   everything_before_bad_keyword_case_sensitive = (/^(.*)(FRENCH)(.*)$/.match(dots_removed) || ['', dots_removed])[1]
+#   everything_before_bad_keyword = (/^(.*)(TRUEFRENCH|DVDRIP|XVID|DIVX)(.*)$/i.match(everything_before_bad_keyword_case_sensitive) || ['', everything_before_bad_keyword_case_sensitive])[1]
+#   everything_before_bad_keyword
+# end
+# 
+# algorithms['R3'] = Proc.new do |fileName|
+#   fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
+#   dots_removed = fileNameWithoutExtension.gsub('.', ' ')
+#   everything_before_bad_keyword_case_sensitive = (/^(.*)(FRENCH)(.*)$/.match(dots_removed) || ['', dots_removed])[1]
+#   everything_before_bad_keyword = (/^(.*)(TRUEFRENCH|DVDRIP|XVID|DIVX)(.*)$/i.match(everything_before_bad_keyword_case_sensitive) || ['', everything_before_bad_keyword_case_sensitive])[1]
+#   everything_before_bad_keyword
+# end
+# 
+# algorithms['R4'] = Proc.new do |fileName|
+#   fileNameWithoutExtension = /^(.*)\.([a-z]){2,4}$/i.match(fileName)[1]
+#   dots_removed = fileNameWithoutExtension.gsub('.', ' ')
+#   everything_before_bad_keyword_case_sensitive = (/^(.*)(FRENCH)(.*)$/.match(dots_removed) || ['', dots_removed])[1]
+#   everything_before_bad_keyword = (/^(.*)(TRUEFRENCH|DVDRIP|XVID|DIVX)(.*)$/i.match(everything_before_bad_keyword_case_sensitive) || ['', everything_before_bad_keyword_case_sensitive])[1]
+#   remove_between_parenthesis = everything_before_bad_keyword.gsub(/\(.*\)/, '')
+#   remove_between_parenthesis
+# end
+# 
+
+
+
+
+# ---------------- DO NOT MODIFY BELOW HERE -------------
 
 
 def rankDifference(originalString, givenString)
@@ -20,20 +84,25 @@ def rankDifference(originalString, givenString)
 end
 
 doc = Nokogiri::HTML(open('MovieLibrary.html'))
-algorithms.each_key do |cleaner_proc_name|
-  cleaner_proc = algorithms[cleaner_proc_name]
+$algorithms.each do |algorithm|
   totalScore = 0.0
   doc.css('#movie_file_names tr').each do |movie|
     original_file_node = movie.css('td:first-child').first
     expected_movie_node =  movie.css('td:nth-child(2)').first
     if (original_file_node != nil && expected_movie_node != nil)
-      score = rankDifference(cleaner_proc.call(original_file_node.content), expected_movie_node.content)
+      original_file_name = original_file_node.content
+      expected_movie_name = expected_movie_node.content
+      computed_movie_name = original_file_name
+      algorithm.each do |block_name|
+        computed_movie_name = $building_blocks[block_name].call(computed_movie_name)
+      end
+      score = rankDifference(computed_movie_name, expected_movie_name)
       totalScore += score
-      puts "#{score} : #{original_file_node.content.inspect} : Got #{cleaner_proc.call(original_file_node.content).inspect}, expected #{expected_movie_node.content.inspect}" if ARGV.member?('v')
+      puts "#{score} : #{original_file_node.content.inspect} : Got #{computed_movie_name.inspect}, expected #{expected_movie_node.content.inspect}" if ARGV.member?('v')
     end
   end
   puts "-----------------" if ARGV.member?('v')
-  puts "TOTAL SCORE for #{cleaner_proc_name.inspect} : (lower is better): #{totalScore.round}"
+  puts "TOTAL SCORE for #{algorithm.inspect} : (lower is better): #{totalScore.round}"
   puts " " if ARGV.member?('v')
   puts " " if ARGV.member?('v')
 end
